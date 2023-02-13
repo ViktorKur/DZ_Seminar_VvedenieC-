3d211c5cafab227c13f3cd38e108e862e9c3f93f
Viktor K <popvik13@mail.ru>
2023-02-14 02:54:12 +0300

    Sozdal rabochii fail C# v papke Zadacha008 i dobavil programu Zadachi №8  DZ

---
+++ DZ_Seminar001/Zadacha008/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+System.Console.Write("Введите число N, для проверки интервала от 1 до N на четность: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num==0 || num<0) 
+{
+  System.Console.WriteLine($"Введенное Вами число {num} не отвечает условию задачи, т.е. не входит в от 1 до N");
+  return;
+}
+System.Console.WriteLine("Ряд четных чисел от от 1 до N: ");
+for (int i=1; i < num+1; i++)
+{
+if (i%2==0) System.Console.Write($"{i}; ");
+}

ad8e18aa072ebfea9a7d4d745ee9bd22ff4b6722
Viktor K <popvik13@mail.ru>
2023-02-22 02:18:10 +0300

    V DZ_Seminar003 Sozdal rabochii fail C# v papke Zadacha021 i dobavil  programu Zadachi №21 podpravil  DZ

---
+++ DZ_Seminar003/Zadacha021/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+double[] CoordArr(string text)
+{
+  double[] CorArr = new double [3];
+  System.Console.WriteLine(text);
+  for (int i = 0; i < 3; i++)
+  {
+    CorArr[i] = Convert.ToDouble(Console.ReadLine());
+  }
+  return CorArr;
+}
+
+double Rastoyin(double[] A, double[] B)
+{
+  double Rast = double.Round(Math.Sqrt(Math.Pow((A[0] - B[0]),2 ) + Math.Pow((A[1] - B[1]),2) + Math.Pow((A[2] - B[2]),2)),2);
+return Rast;
+}
+
+double[] TA = new double[3]; double[] TB = new double[3];
+TA = CoordArr("Введите координаты точки А(X,Y,Z)");
+TB = CoordArr("Введите координаты точки B(X,Y,Z)");
+Console.WriteLine("Расстояние между точками A(" + string.Join(", ", TA) + ") и B(" + string.Join(", ", TB) + ") = " + string.Join("", Rastoyin(TA, TB)));
+

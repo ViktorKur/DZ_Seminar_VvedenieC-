a70945b796e304ffd69f56086a4d34de1aed4e8c
Viktor K <popvik13@mail.ru>
2023-02-22 23:32:30 +0300

    V DZ_Seminar004 Sozdal rabochii fail C# v papke Zadacha027 i dobavil  programu Zadachi №27 podpravil  DZ

---
+++ DZ_Seminar004/Zadacha027/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Clear();
+int VvodDan(string text)
+{
+  System.Console.WriteLine(text);
+  int chislo = Convert.ToInt32(Console.ReadLine());
+  return chislo;
+}
+int Sum (int Ch)
+{
+  int Ost = 0;
+  while (Ch > 0 )
+  {
+    Ost=Ost+Ch%10;
+    Ch=Ch/10; 
+  }
+   return Ost;
+}
+
+void Print (int Zn, int Sum)
+{
+  System.Console.WriteLine($"Введенное Вами число ({Zn}), сумма входящих в него чисел = {Sum} ");
+}
+
+
+void main(int Zn)
+{
+if (Zn/10>=0) 
+{
+  int SumCh = Sum(Zn);
+  Print (Zn, SumCh);
+ }
+ else 
+ {
+  int SumCh = Sum( Math.Abs(Zn));
+  Print (Zn, SumCh*-1);
+ }
+}
+
+int Zn = VvodDan("Введите пожалуйста любое целое число : ");
+main (Zn);

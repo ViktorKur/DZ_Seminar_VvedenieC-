818caefa03f77ab5f8ee6be70bb3b3e41b17df98
Viktor K <popvik13@mail.ru>
2023-02-22 03:34:14 +0300

    V DZ_Seminar003 Sozdal rabochii fail C# v papke Zadacha023 i dobavil  programu Zadachi №23 podpravil  DZ

---
+++ DZ_Seminar003/Zadacha023/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+int VhodZn(string text)
+{
+  System.Console.WriteLine(text);
+  int N = Convert.ToInt32(Console.ReadLine());
+  return N;
+}
+
+double[] TablKub(int num)
+{
+  double[] kub = new double[num];
+  for (int i = 0; i < num; i++)
+  {
+    kub[i] = Math.Pow((i + 1), 3);
+  }
+  return kub;
+}
+
+void PrintTab(int Zn)
+{
+  if (Zn > 0) System.Console.WriteLine($"От введенного Вами числа {Zn}, таблица кубов = >  " + String.Join(", ", TablKub(Zn)));
+   else System.Console.WriteLine("Число N должно быть больше нуля, попробуйте ещё раз");
+}
+
+  int N = VhodZn("Введите число (N) для расчета таблицы кубов чисел от 1 до N : ");
+  PrintTab(N);
